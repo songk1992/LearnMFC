d68ae1dd00f99c9be1d51a53df196e793d302e24
songk1992 <zuppiy@naver.com>
2021-05-10 22:01:23 +0900

    초기 커밋.

---
+++ LearnMFCView.h
@@ -0,0 +1,46 @@
+﻿
+// LearnMFCView.h: CLearnMFCView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CLearnMFCView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CLearnMFCView() noexcept;
+	DECLARE_DYNCREATE(CLearnMFCView)
+
+// 특성입니다.
+public:
+	CLearnMFCDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 구현입니다.
+public:
+	virtual ~CLearnMFCView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // LearnMFCView.cpp의 디버그 버전
+inline CLearnMFCDoc* CLearnMFCView::GetDocument() const
+   { return reinterpret_cast<CLearnMFCDoc*>(m_pDocument); }
+#endif
+
